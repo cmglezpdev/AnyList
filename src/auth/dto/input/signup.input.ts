483b21d163e8b805af4da53eb4142498eddbb91c
Carlos Manuel González <cmglezpdev@gmail.com>
2023-07-30 19:21:42 -0400

    feat: add input dto for handle singup of user

---
+++ src/auth/dto/input/signup.input.ts
@@ -0,0 +1,23 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { IsEmail, IsNotEmpty, IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
+@InputType()
+export class SignUpInput {
+    @Field(() => String)
+    @IsEmail()
+    email: string;
+    
+    @Field(() => String)
+    @IsNotEmpty()
+    fullName: string;
+    
+    @Field(() => String)
+    @IsString()
+    @MinLength(6)
+    @MaxLength(50)
+    @Matches(
+        /(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+        message: 'The password must have a Uppercase, lowercase letter and a number'
+    })
+    password: string;
+}
