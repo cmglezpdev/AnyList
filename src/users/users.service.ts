221c3af9f56bfdf1749b17198c301caf2c60833d
Carlos Manuel González <cmglezpdev@gmail.com>
2023-07-30 18:25:03 -0400

    chore: add firm and remove innecesary methods of the crud

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserInput } from './dto/create-user.input';
-import { UpdateUserInput } from './dto/update-user.input';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserInput: CreateUserInput) {
-    return 'This action adds a new user';
+
+  async findAll(): Promise<User[]> {
+    throw new Error(`findAll not implemented`)
   }
 
-  findAll() {
-    return `This action returns all users`;
+  findOne(id: string): Promise<User> {
+    throw new Error(`findOne not implemented`)
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserInput: UpdateUserInput) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  block(id: string): Promise<User> {
+    throw new Error(`block not implemented`)
   }
 }
