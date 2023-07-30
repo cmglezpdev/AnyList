8659b9739f3366218dd5b9263c1d0b500783f564
Carlos Manuel González <cmglezpdev@gmail.com>
2023-07-30 17:48:05 -0400

    feat: find items from db

--- src/items/items.resolver.ts
+++ src/items/items.resolver.ts
@@ -1,4 +1,6 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { ParseUUIDPipe } from '@nestjs/common';
+import { Resolver, Query, Mutation, Args, Int, ID } from '@nestjs/graphql';
+
 import { ItemsService } from './items.service';
 import { Item } from './entities/item.entity';
 import { UpdateItemInput, CreateItemInput } from './dto/input';
@@ -15,12 +17,14 @@ export class ItemsResolver {
   }
 
   @Query(() => [Item], { name: 'items' })
-  findAll() {
+  async findAll(): Promise<Item[]> {
     return this.itemsService.findAll();
   }
 
   @Query(() => Item, { name: 'item' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
+  findOne(
+    @Args('id', { type: () => ID }, ParseUUIDPipe) id: string
+  ) {
     return this.itemsService.findOne(id);
   }
 
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -18,12 +18,14 @@ export class ItemsService {
     return await this.itemRepository.save(item);
   }
 
-  findAll() {
-    return `This action returns all items`;
+  async findAll(): Promise<Item[]> {
+      return this.itemRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} item`;
+  async findOne(id: string): Promise<Item> {
+    const item = await this.itemRepository.findOneBy({ id });
+    if(!item) throw new NotFoundException(`Item with id ${item} not found`);
+    return item;
   }
 
   update(id: number, updateItemInput: UpdateItemInput) {
