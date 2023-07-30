961c8cd4447b527403ecc0ffa780eacfa2c6bd78
Carlos Manuel González <cmglezpdev@gmail.com>
2023-07-30 16:43:10 -0400

    chore: add base project with items CRUD (GraphQL)

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+
+import { ItemsModule } from './items/items.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()]
+    }),
+    ItemsModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
