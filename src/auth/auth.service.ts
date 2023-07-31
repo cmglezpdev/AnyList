eee72a8594280144c96bf3afae3f9351f3543d86
Carlos Manuel González <cmglezpdev@gmail.com>
2023-07-30 23:43:29 -0400

    feat: create jwts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,27 +1,26 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 
 import { AuthResponse } from './types';
 import { SignInInput, SignUpInput } from './dto/input';
-import { User } from '../users/entities/user.entity';
-import { UsersService } from 'src/users/users.service';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
 
-    constructor(
-        @InjectRepository(User)
-        private readonly usersRepostory: Repository<User>,
-        private readonly usersService: UsersService,
-    ){}
+  constructor(
+      private readonly usersService: UsersService,
+      private readonly jwtService: JwtService
+  ){}
+
+  private getJwtToken(id: string) {
+    return this.jwtService.sign({ id });
+  }  
 
   async signUp(signUpInput: SignUpInput): Promise<AuthResponse> {
     const user = await this.usersService.create(signUpInput);
-    const token = "sgdgdfgfddfdgdd.hfg.sd.gdfhgsdgdfggdfhg-fgd/sdgdf";
-    
-    // TODO: create JWT
+    const token = this.getJwtToken(user.id);
     return {user, token};
   }
 
@@ -31,9 +30,7 @@ export class AuthService {
 
     const validPassword = bcrypt.compareSync(password, user.password);
     if(!validPassword) throw new BadRequestException('Email / Password do not match');
-    
-    // TODO: create JWT
-    const token = "sgfsdgdgdfhfg.g.dfh.df.hs.g.dfghdfhdf";
+    const token = this.getJwtToken(user.id);
     return {user, token}; 
   }
 }
