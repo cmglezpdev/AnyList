3071a8764350a16c75dd9d3ba5db89e266716095
Carlos Manuel González <cmglezpdev@gmail.com>
2023-07-31 01:11:04 -0400

    feat: register Valid Roles like a GraphQL Enum

--- src/auth/enums/valid-roles.enum.ts
+++ src/auth/enums/valid-roles.enum.ts
@@ -1,6 +1,12 @@
+import { registerEnumType } from "@nestjs/graphql";
 
 export enum ValidRoles {
     user = 'user',
     admin = 'admin',
     superUser = 'super-user'
 }
+
+registerEnumType(ValidRoles, { 
+    name: "ValidRoles", 
+    description: "Valid roles for the users" 
+})
