5a55fb79e944b5caa8b5b2aac8dedf3ac31dca5e
Carlos Manuel González <cmglezpdev@gmail.com>
2023-07-31 01:32:59 -0400

    feat: add authorization to use queries of user resolve

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,31 +1,39 @@
+import { UseGuards, ParseUUIDPipe } from '@nestjs/common';
 import { Resolver, Query, Mutation, Args, ID } from '@nestjs/graphql';
 
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { FindAllArgs } from './dto/args';
+import { JwtAuthGuard } from 'src/auth/guards';
+import { GetUser } from '../auth/decorators';
+import { ValidRoles } from '../auth/enums';
 
 @Resolver(() => User)
+@UseGuards(JwtAuthGuard)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
   @Query(() => [User], { name: 'users' })
   findAll(
-    @Args() findAllArgs: FindAllArgs
+    @Args() findAllArgs: FindAllArgs,
+    @GetUser([ValidRoles.admin, ValidRoles.superUser]) user: User
   ): Promise<User[]> {
     return this.usersService.findAll(findAllArgs);
   }
 
   @Query(() => User, { name: 'user' })
+  
   findOne(
-    @Args('id', { type: () => ID }) id: string
+    @Args('id', { type: () => ID }, ParseUUIDPipe) id: string,
+    @GetUser([ValidRoles.admin, ValidRoles.superUser]) user: User
   ): Promise<User> {
-      throw new Error('findOne Query not implement');
-    // return this.usersService.findOne(id);
+    return this.usersService.findOneById(id);
   }
 
   @Mutation(() => User)
   blockUser(
-    @Args('id', { type: () => ID }) id: string
+    @Args('id', { type: () => ID }, ParseUUIDPipe) id: string,
+    @GetUser([ValidRoles.admin]) user: User
   ): Promise<User> {
     return this.usersService.block(id);
   }
