a8226670ae5becb70aa2352ba2446b8ed8ac65cb
Carlos Manuel González <cmglezpdev@gmail.com>
2023-07-31 20:43:49 -0400

    feat: add List entity with typorm

--- src/lists/entities/list.entity.ts
+++ src/lists/entities/list.entity.ts
@@ -1,14 +1,16 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 import { User } from '../../users/entities/user.entity';
 
 @ObjectType()
-@Entity()
+@Entity("lists")
 export class List {
 
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column('text')
   name: string;
 
   // Relation, index('user-id-list-index')
--- src/lists/lists.module.ts
+++ src/lists/lists.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { ListsService } from './lists.service';
 import { ListsResolver } from './lists.resolver';
+import { List } from './entities/list.entity';
 
 @Module({
-  providers: [ListsResolver, ListsService]
+  providers: [ListsResolver, ListsService],
+  imports: [
+    TypeOrmModule.forFeature([ List ])
+  ]
 })
 export class ListsModule {}
